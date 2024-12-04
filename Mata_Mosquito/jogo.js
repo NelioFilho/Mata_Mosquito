8ba17a48af7998be5366120b668f7d68a60d5692
Nélio <neliofilhodg@outlook.com>
2024-12-04 20:56:11 -0300

    tamanho mosquito e vidas

--- Mata_Mosquito/jogo.js
+++ Mata_Mosquito/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -11,6 +12,19 @@ ajustaTamanhoPalcoJogo();
 
 
 function posicaoRandomica(){
+    
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+        
+        if( vidas > 3){
+            alert('Game Over')
+        }else{
+            document.getElementById('v' + vidas).src='imagens/coracao_vazio.png'
+
+            vidas++
+        }
+    }
+
     var posicaoX = Math.floor(Math.random() * largura) - 90;
     var posicaoY = Math.floor(Math.random() * altura) - 90;
 
@@ -21,10 +35,44 @@ function posicaoRandomica(){
 
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
+
+    console.log(tamanhoAleatorio())
+}
+
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+            
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 2)
+
+    switch(classe){
+        case 0:
+            return 'ladoA'
+            
+        case 1:
+            return 'ladoB'
+    }
 }
